230471b1cc0c4f27523f78a2607260ed53da4f0f
EfratGrinboom <shifraefrat@gmail.com>
2022-09-18 02:43:22 +0300

    initial code+welcome functions

---
+++ targil1/Program7968.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome7968();
+            Welcome2703();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome2703();
+        private static void Welcome7968()
+        {
+            Console.WriteLine("Enter your name: ");
+            string user = Console.ReadLine();
+            Console.WriteLine(user, ", welcome to my first console application");
+        }
+    }
+}
